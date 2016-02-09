315559bcad50bb9c58a152ea5abf922861af78b5
James Jones <jamjon3@gmail.com>
2016-02-08 19:21:07 -0500

    Initial commit

---
+++ src/GeorgiaCreateSchemaCommand.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+namespace gcg\votertools;
+
+use Symfony\Component\Console\Command\Command;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Formatter\OutputFormatterStyle;
+/**
+ * Description of GeorgiaCreateSchemaCommand
+ *
+ * @author jam
+ */
+class GeorgiaCreateSchemaCommand extends Command {
+    //put your code here
+}
