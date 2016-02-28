6411b0c55668678ab4633afc8626743cc0fa4fdc
James Jones <jamjon3@gmail.com>
2016-02-28 12:12:58 -0500

    Created a working raw history LOAD DATA SQL file

---
+++ sql/florida/HistoryLoadImport.sql
@@ -0,0 +1,12 @@
+LOAD DATA LOCAL INFILE '{$filename}'
+    IGNORE INTO TABLE `Histories`
+    FIELDS TERMINATED BY '\t'
+    LINES TERMINATED BY '\n'
+    (`county_code`,
+    `voter_id`,
+    @Election_Date,
+    `election_type`,
+    `history_code`)
+    SET `export_date` = STR_TO_DATE('{$filedate}','%Y%m%d'),
+    `election_date` = STR_TO_DATE(@Election_Date,'%m/%d/%Y');
+
