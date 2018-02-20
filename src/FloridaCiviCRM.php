d5c6015d0b56320a8dae3f6d340044497b71ca22
James Jones <jamjon3@gmail.com>
2018-02-19 21:22:16 -0500

    Backing out some code

--- src/FloridaCiviCRM.php
+++ src/FloridaCiviCRM.php
@@ -18,14 +18,14 @@ trait FloridaCiviCRM {
     $votertable = $this->config['voter']['florida']['civicrm']['tablename'];
     $voterregistrationIDfield = \array_flip($this->config['voter']['florida']['civicrm']['voterFieldMap'])[$this->config['voter']['florida']['civicrm']['voterIDfield']];
     $voterids = [];
-    if($voters = $this->getConnection($this->connectionName)->select($votertable,[
-      "$voterregistrationIDfield"
-    ], 
-    Medoo::raw('WHERE LENGTH(<' . $voterregistrationIDfield . '>) > 5'))) {
-      return [$voters];
-    } else {
-      return [];
-    }
-    return [ 'test' ];
+//    if($voters = $this->getConnection($this->connectionName)->select($votertable,[
+//      "$voterregistrationIDfield"
+//    ], 
+//    Medoo::raw('WHERE LENGTH(<' . $voterregistrationIDfield . '>) > 5'))) {
+//      return [$voters];
+//    } else {
+//      return [];
+//    }
+    return [ $votertable, $voterregistrationIDfield ];
   }
 }
